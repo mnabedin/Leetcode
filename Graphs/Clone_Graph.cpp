c276d1bb2118847683fc5e0bd4703b8443fd6a24
Nafis Abedin <nafisabedin@Nafiss-MacBook-Pro.local>
2022-04-26 00:31:18 -0400

    add clone graph

--- Graphs/Clone_Graph.cpp
+++ Graphs/Clone_Graph.cpp
@@ -1,1 +1,21 @@
-Clone_Graph.cpp
+class Solution {
+public:
+	unordered_map<Node*, Node*> mp;
+    Node* cloneGraph(Node* node) {
+    	if (!node)
+    		return nullptr;
+    	mp = {};
+        return dfs(node);
+    }
+
+    Node* dfs(Node* node) {
+    	if (mp.find(node) != mp.end())
+    		return mp[node];
+    	Node* new_node = new Node(node -> val);
+    	mp[node] = new_node;
+    	for (auto nei : node -> neighbors) {
+    		mp[node] -> neighbors.push_back(dfs(nei));
+    	}
+    	return new_node;
+    }
+};
